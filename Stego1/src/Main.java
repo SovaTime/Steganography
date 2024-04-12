094b3e85442f32a53498c7d2ddabd6cb8ed44462
SovaTime <121239383+SovaTime@users.noreply.github.com>
2024-04-12 20:00:59 +0300

    Add files via upload

--- Stego1/src/Main.java
+++ Stego1/src/Main.java
@@ -1,35 +1,26 @@
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
-
+import javax.imageio.ImageIO;
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        String message = "The LSB method. LSB (Least Significant Bit, least significant bit) — " +
-                "the essence of this method is to replace the last significant bits in the container " +
-                "(images, audio or video recordings) with the bits of the hidden message.";
-        String keyFile = "key.txt"; // Сохранение псевдослучайной последовательности
-        encryption("original.JPG", message);
-    }
-
-    public static String toBinary(String message){
-        byte[] bytes = message.getBytes(); // Преобразование в массив байтов
-        StringBuilder binaryMess = new StringBuilder();
-        // Преобразование каждого байта в восьмеричное представление
-        for (byte b : bytes) {
-            binaryMess.append(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
-        }
-        // Добавление последовательности в конец сообщения
-        binaryMess.append("1111111111111110");
-        return binaryMess.toString();
-    }
-
     private static ArrayList<Point> traversalOrder;
-    // Создание псевдослучайного порядка
-    public static ArrayList<Point> generateRandomTO(int width, int height) {
+    // Сообщение в бинарную строку с фиксированной последовательностью в конце
+    public static String toBinary(String message) {
+        byte[] bytes = message.getBytes(); // Преобразование в массив байтов
+        StringBuilder binaryMessage = new StringBuilder();
+        // Преобразование байта в восьмеричное представление и добавление к общей бинарной строке
+        for (byte b : bytes) {
+            binaryMessage.append(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+        }
+        // Фиксированная последовательности в конец сообщения
+        binaryMessage.append("1111111111111110");
+        return binaryMessage.toString();
+    }
+    // Создание псевдослучайного порядка обхода пикселей
+    public static ArrayList<Point>
+    generateRandomTO(int width, int height) {
         ArrayList<Point> order = new ArrayList<>();
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
@@ -43,11 +34,12 @@ public class Main {
     private static void saveTO(String file, ArrayList<Point> order) throws IOException {
         try (PrintWriter writer = new PrintWriter(file)) {
             for (Point point : order) {
-                writer.println((int) point.getX() + " " + (int) point.getY());
+                writer.println((int) point.getX() + " " + (int)
+                        point.getY());
             }
         }
     }
-    // Загрузка из файла
+    // Загрузка псевдослучайного порядка
     private static ArrayList<Point> loadTO(String file) throws IOException {
         ArrayList<Point> order = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -62,9 +54,8 @@ public class Main {
         return order;
     }
 
-    public static void encryption (String image, String message) throws IOException {
+    public static void encrypt(String image, String message) throws IOException {
         BufferedImage img = ImageIO.read(new File(image));
-        // byte[] pixels = ((DataBufferByte) encryptImg.getRaster().getDataBuffer()).getData();
         int width = img.getWidth();
         int height = img.getHeight();
         String binaryMess = toBinary(message);
@@ -73,14 +64,14 @@ public class Main {
         saveTO("key.txt", traversalOrder);
 
         int index = 0;
-        // Перебор всех пикселей изображения в псевдослучайном порядке и встраивание битов сообщения в младшие биты каждой компоненты RGB
+        // Перебор всех пикселей изображения в псевдослучайном порядке и встраивание битов сообщения в младшие биты
         for (Point point : traversalOrder) {
             int x = (int) point.getX();
             int y = (int) point.getY();
             int pixel = img.getRGB(x, y);
             for (int i = 16; i >= 0; i -= 8) {
                 if (index < binaryMess.length()) {
-                    // Извлекаем текущий бит из бинарной строки сообщения, обнуляем младший для встраивания и меняем бит
+                    // Извлекаем текущий бит, обнуляем младший для встраивания и меняем бит
                     int bit = Character.getNumericValue(binaryMess.charAt(index));
                     pixel = (pixel & ~(1 << i));
                     pixel |= (bit << i);
@@ -88,16 +79,60 @@ public class Main {
                 }
             }
             img.setRGB(x, y, pixel);
-            // Если сообщение закодировано, сохраняем изображение и завершаем метод
+            // Когда закодировано, сохраняем изображение и завершаем метод
             if (index >= binaryMess.length()) {
-                ImageIO.write(img, "JPG", new
-                        File("encode.JPG"));
+                ImageIO.write(img, "png", new File("encrypt.png"));
                 return;
             }
         }
     }
 
-    public static void decryption (BufferedImage img){
+    public static String decrypt(String image, String keyF) throws IOException {
+        ArrayList<Point> traversalOrder = loadTO(keyF);
+        BufferedImage img = ImageIO.read(new File(image));
+        StringBuilder binaryMess = new StringBuilder();
+        int stopIndex = -1;
+        boolean flag = false;
 
+        // Перебор всех пикселей в сохраненном порядке и извлечение младших бит
+        for (Point point : traversalOrder) {
+            int x = (int) point.getX();
+            int y = (int) point.getY();
+            int pixel = img.getRGB(x, y);
+            for (int i = 16; i >= 0; i -= 8) {
+                binaryMess.append((pixel >> i) & 1);
+                if (binaryMess.length() >= 16 &&
+                        binaryMess.substring(binaryMess.length() - 16).equals("1111111111111110")) {
+                    flag = true;
+                    stopIndex = binaryMess.length() - 16;
+                    break;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+        // Когда конец сообщения, декодируем бинарное сообщение в строку
+        if (stopIndex != -1) {
+            binaryMess.delete(stopIndex, binaryMess.length());
+            StringBuilder message = new StringBuilder();
+            for (int i = 0; i < binaryMess.length(); i += 8) {
+                int endIndex = Math.min(i + 8, binaryMess.length());
+                int byteValue = Integer.parseInt(binaryMess.substring(i, endIndex), 2);
+                message.append((char) byteValue);
+            }
+            return message.toString();
+        } else {
+            System.out.println("Encoded message not found.");
+            return "";
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String message = "algoritm LSB";
+        String keyFile = "key.txt"; // Сохранение псевдослучайной последовательности
+        encrypt("original.JPG", message);
+        String decodedMessage = decrypt("encrypt.png", keyFile);
+        System.out.println("Hidden message: " + decodedMessage);
     }
 }
